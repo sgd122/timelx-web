599399f482f449504f40f76e047da2a4dcf46e18
Seong Gi Dong <sgd0947@gmail.com>
2025-01-10 17:56:11 +0900

    feat(search): 검색 플로팅 버튼 구현

---
+++ src/components/ui/FloatingSearchButton/index.tsx
@@ -0,0 +1,28 @@
+import { Button, Tooltip } from '@radix-ui/themes';
+import Link from 'next/link';
+import { FaSearch } from 'react-icons/fa';
+
+const FloatingSearchButton = () => {
+  return (
+    <div className="absolute bottom-6 right-6">
+      <Tooltip content="검색하기">
+        <Button
+          asChild={true}
+          variant="solid"
+          color="yellow"
+          size="2"
+          radius="full"
+          className="w-[48px] h-[48px]"
+          onClick={() => console.log('Search button clicked!')}
+          aria-label="Search"
+        >
+          <Link href="/search">
+            <FaSearch size={24} />
+          </Link>
+        </Button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default FloatingSearchButton;
