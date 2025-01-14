02db252e3182992da7e8fd50c7926731296337a1
Dong-Young Kim <31337.persona@gmail.com>
2025-01-14 17:19:55 +0900

    feat: `SearchBar` 컴포넌트 추가
    Resolves #20

---
+++ src/components/ui/SearchBar/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Text } from '@radix-ui/themes';
+import type React from 'react';
+
+import { cn } from '@/lib/utils';
+
+type SearchBarProps = React.PropsWithChildren & {
+  className?: string;
+};
+
+/**
+ * @example
+ * ```tsx
+ * <SearchBar>
+ *   2025년 02월 08일 서울의 전시회 시간을 밝혀줘
+ * </SearchBar>
+ * ```
+ */
+const SearchBar: React.FC<SearchBarProps> = ({ children, className }) => {
+  return (
+    <Box
+      className={cn(
+        'block',
+        'w-fit max-w-[80%]',
+        'ml-auto',
+        'px-4 py-2',
+        'rounded-full',
+        'bg-tx-white text-tx-black',
+        className
+      )}
+    >
+      <Text>{children}</Text>
+    </Box>
+  );
+};
+
+export default SearchBar;
