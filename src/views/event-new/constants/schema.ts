7efc5ee1c91ad58f16dfe9b1d72f77c920b6dd57
Seong Gi Dong <sgd0947@gmail.com>
2025-01-15 18:03:25 +0900

    feat(event-new): 이미지 validation 처리 부분 추가

--- src/views/event-new/constants/schema.ts
+++ src/views/event-new/constants/schema.ts
@@ -3,15 +3,28 @@ import * as z from 'zod';
 export const schema = z.object({
   title: z.string().min(1, '이벤트 제목을 입력해주세요.'),
   image: z
-    .instanceof(File)
-    .refine(
-      (file) => file.size <= 5 * 1024 * 1024,
-      '이미지 크기는 5MB 이하이어야 합니다.'
-    ) // 크기 제한
-    .refine(
-      (file) => ['image/jpeg', 'image/png', 'image/gif'].includes(file.type),
-      '이미지는 JPEG, PNG, GIF 형식만 지원합니다.'
-    ),
+    .unknown() // 어떤 값이든 받아들이는 기본 타입
+    .superRefine((val, ctx) => {
+      if (!(val instanceof File)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: '이미지가 업로드되지 않았습니다.', // 원하는 메시지 설정
+        });
+        return;
+      }
+      if (val.size > 5 * 1024 * 1024) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: '이미지 크기는 5MB 이하이어야 합니다.',
+        });
+      }
+      if (!['image/jpeg', 'image/png', 'image/gif'].includes(val.type)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: '이미지는 JPEG, PNG, GIF 형식만 지원합니다.',
+        });
+      }
+    }),
   startDate: z
     .string()
     .min(1, '시작일을 입력해주세요.')
