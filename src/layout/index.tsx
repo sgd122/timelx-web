3acc25d05a22cb19d015125abe2b815ad85454d4
Seong Gi Dong <sgd0947@gmail.com>
2025-02-01 15:08:03 +0900

    feat(layout): 필요없는 코드 제거

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,7 +4,6 @@ import type { PropsWithChildren } from 'react';
 import useViewportHeight from '@/shared/hooks/useViewportHeight';
 import { ScrollAreaProvider } from '@/shared/providers/ScrollAreaProvider';
 import FloatingSearchButton from '@/shared/ui/FloatingSearchButton';
-import { LogScreen } from '@/shared/ui/LogScreen';
 import Footer from '@/widgets/Footer';
 import Header from '@/widgets/Header';
 import LeftContainer from '@/widgets/layout/ui/LeftContainer';
@@ -13,43 +12,41 @@ const Layout = ({ children }: PropsWithChildren) => {
   const viewportHeight = useViewportHeight();
 
   return (
-    <LogScreen>
-      <Box
-        maxWidth={'1536px'}
-        style={{
-          margin: '0 auto',
-          display: 'flex',
-          alignItems: 'center',
-        }}
+    <Box
+      maxWidth={'1536px'}
+      style={{
+        margin: '0 auto',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <LeftContainer className="hidden max-layout:flex mx-14" />
+      <Container
+        size="1"
+        className="flex items-center min-layout:items-start min-layout:w-[448px]"
       >
-        <LeftContainer className="hidden max-layout:flex mx-14" />
-        <Container
-          size="1"
-          className="flex items-center min-layout:items-start min-layout:w-[448px]"
+        <Box
+          style={{
+            background: 'var(--gray-a2)',
+            position: 'relative',
+            overflow: 'hidden',
+            height: viewportHeight,
+            display: 'flex',
+            flexDirection: 'column',
+            paddingBottom: '16px',
+          }}
         >
-          <Box
-            style={{
-              background: 'var(--gray-a2)',
-              position: 'relative',
-              overflow: 'hidden',
-              height: viewportHeight,
-              display: 'flex',
-              flexDirection: 'column',
-              paddingBottom: '16px',
-            }}
-          >
-            <Header />
-            <Box className="flex-1 overflow-y-hidden">
-              <ScrollAreaProvider>
-                <Box className="px-6 pb-4">{children}</Box>
-                <FloatingSearchButton />
-              </ScrollAreaProvider>
-            </Box>
-            <Footer />
+          <Header />
+          <Box className="flex-1 overflow-y-hidden">
+            <ScrollAreaProvider>
+              <Box className="px-6 pb-4">{children}</Box>
+              <FloatingSearchButton />
+            </ScrollAreaProvider>
           </Box>
-        </Container>
-      </Box>
-    </LogScreen>
+          <Footer />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
