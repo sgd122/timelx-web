798e851015314ab8e6abdd64648ab90f61a73287
Seong Gi Dong <sgd0947@gmail.com>
2025-02-03 13:23:05 +0900

    feat(layout): 아이콘에 link 추가

--- src/widgets/layout/ui/LeftContainerHeader.tsx
+++ src/widgets/layout/ui/LeftContainerHeader.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import LogoTitle from '@/shared/assets/icon/title.png';
 
 const LeftContainerHeader = () => (
-  <div className="flex items-center text-center">
-    <Image src={LogoTitle} alt="logo" height={20} />
-    <span className="text-md font-semibold tracking-wide">타임럭스</span>
-    <span className="bg-gray-700 text-xs rounded-full px-2 py-1 ml-2">
-      Beta
-    </span>
-  </div>
+  <Link href="/">
+    <div className="flex items-center text-center">
+      <Image src={LogoTitle} alt="logo" height={20} />
+      <span className="text-md font-semibold tracking-wide">타임럭스</span>
+      <span className="bg-gray-700 text-xs rounded-full px-2 py-1 ml-2">
+        Beta
+      </span>
+    </div>
+  </Link>
 );
 
 export default LeftContainerHeader;
